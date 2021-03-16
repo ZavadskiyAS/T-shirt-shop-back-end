8ee1b6592f371dad72636eef217c18050f0f4e57
ZavadskiyAS <Zavadskiyweb@gmail.com>
2021-03-17 00:42:18 +0200

    added mongodb

--- index.js
+++ index.js
@@ -1,10 +1,32 @@
 const express = require('express')
+const mongoose = require('mongoose')
+const exphbs = require('express-handlebars')
 
 // const PORT = process.evn.PORT || 5000
 const PORT = 3000
 
 const app = express()
-
-app.listen(PORT, () => {
-    console.log('servr up...');
+const hbs = exphbs.create({
+    defaultLayout: 'main',
+    extname: 'hbs'
 })
+
+app.engine('hbs', hbs.engine)
+app.set('view engine', 'hbs')
+app.set('views', 'views')
+
+async function start() {
+    try {
+        await mongoose.connect('', {
+            useNewUrlParser: true,
+            useFindAndModify: false
+        })
+        app.listen(PORT, () => {
+            console.log('servr up...');
+        })
+    } catch(e){
+        console.log(e);
+    }
+}
+
+start()
