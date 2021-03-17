ed7a1cc70b4ad4cc77969a624080dbf1c7eb3360
ZavadskiyAS <Zavadskiyweb@gmail.com>
2021-03-17 02:03:01 +0200

    + hbs

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const mongoose = require('mongoose')
 const exphbs = require('express-handlebars')
+const todoRoutes = require('./routes/todos')
 
 // const PORT = process.evn.PORT || 5000
 const PORT = 3000
@@ -15,9 +16,11 @@ app.engine('hbs', hbs.engine)
 app.set('view engine', 'hbs')
 app.set('views', 'views')
 
+app.use(todoRoutes)
+
 async function start() {
     try {
-        await mongoose.connect('', {
+        await mongoose.connect('mongodb+srv://Zavadskiy:@cluster0.usaau.mongodb.net/todos', {
             useNewUrlParser: true,
             useFindAndModify: false
         })
